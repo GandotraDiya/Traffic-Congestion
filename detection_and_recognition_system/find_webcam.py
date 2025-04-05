4419bfe04cde609e28380cd568bea33b5e9076e2
Shreya Joshi <77975782+shreyaj1@users.noreply.github.com>
2025-04-05 16:52:21 +0530

    Delete detection_and_recognition_system directory

--- detection_and_recognition_system/find_webcam.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-for i in range(5):
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"Webcam found at index: {i}")
-        cap.release()
-
-cv2.destroyAllWindows()
